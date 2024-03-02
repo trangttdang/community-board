03a08bb4780eded952a6bf87a93824bef28da0de
Trang Dang <trangttdang01@gmail.com>
2024-03-02 02:42:14 -0500

    add Header and update Readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import CardList from './components/CardList';
 import cards from './assets/json/cards.json';
+import Header from './components/Header';
 const App = () => {
   return (
+    <>
+    <Header/>
     <div className="App">
+      <br/>
       <h1>🌸🔥 BTS Playlist 🔥🌸</h1>
       <br/>
       <CardList cards={cards} /> 
     </div>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <img src="src/assets/images/header.png" alt="Header Pic" />
+        </header>
+    );
+}
+
+export default Header;
